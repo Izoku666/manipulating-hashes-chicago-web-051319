9e9aa461847f805f5d32337c88231dcbc2c8e3a2
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-18 22:20:24 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-
+  contacts["John Snow"][:favorite_icecream_flavors] << "mint chip"
 
   #remember to return your newly altered contacts hash!
   contacts
